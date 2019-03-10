491ae74823d4ca6b934b742ffe5071d87cba4f63
dfzone <dfzone@sina.com>
2019-03-10 14:19:08 +0800

    修改

--- AspNetCore.Docker/build/build.sh
+++ AspNetCore.Docker/build/build.sh
@@ -10,4 +10,15 @@ cd ../publish
 
 echo publish success
 
-docker build -t aspnetcoredocker .
+read -p "Press any key to continue."
+
+ssh -p 22 root@192.168.163.128 "mkdir -p /data/websites/app"
+
+# 发布
+scp -P 22 -r ../publish/* root@192.168.163.128:/data/websites/app
+
+read -p "Press any key to continue."
+
+ssh -p 22 root@192.168.163.128 "docker build -t aspnetcoredocker -f /data/websites/app/Dockerfile /data/websites/app"
+
+ssh -p 22 root@192.168.163.128 "docker run --name=mydockertest -p 8080:8080 -d  aspnetcoredocker"
